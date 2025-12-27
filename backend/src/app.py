27dd51010dd59d49a24515417aab57f29891758a
nhankhtn <nguyenanhnguyen3006@gmail.com>
2025-12-28 02:29:05 +0700

    update cors

--- backend/src/app.py
+++ backend/src/app.py
@@ -65,6 +65,7 @@ app.add_middleware(
         "http://localhost:3000",
         "http://localhost:3001",
         "http://127.0.0.1:3000",
+        "https://dailybriefai-cyan.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
